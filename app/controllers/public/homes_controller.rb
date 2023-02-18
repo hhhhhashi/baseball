54a427028ea9015f8d24cf37f682238686aa3015
hhhhhashi <r.hashiwbc8091025@gmail.com>
2023-02-18 09:03:49 +0000

    [update]募集要項レイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @recruits =Recruit.left_joins(:member).where(members:{ is_deleted: false })
+    @recruits =Recruit.left_joins(:member).where(members:{ is_deleted: false }).page(params[:page])
   end
 
   def about
